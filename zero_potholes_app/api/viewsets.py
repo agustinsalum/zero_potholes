68fb03c5707d5fd4b347e63b8015d5b5e9dc7a3e
Agustin Salum <agustinsalum92@hotmail.com>
2025-06-23 21:34:07 -0300

    A private endpoint was added to the ReportViewSet that allows moderators to change the status of reports

--- zero_potholes_app/api/viewsets.py
+++ zero_potholes_app/api/viewsets.py
@@ -23,14 +23,16 @@ class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
 
-    # Allows the authenticated user to list their assigned reports
+    """ Allows the authenticated user to list their assigned reports """
+
     @action(detail=False, methods=['get'], url_path='assigned')
     def assigned_to_me(self, request):
         reports = Report.objects.filter(user=request.user)
         serializer = self.get_serializer(reports, many=True)
         return Response(serializer.data)
     
-    # Allows the authenticated moderator to assign a report to themselves
+    """ Allows the authenticated moderator to assign a report to themselves """
+
     # PK should be None; "detail=True" means the action operates on a specific instance (requires an object in the URL)
     # detail=True because this action works with a specific report
     @action(detail=True, methods=['post'])
@@ -46,6 +48,27 @@ class ReportViewSet(viewsets.ModelViewSet):
         report.save()
 
         return Response({'detail': 'Report assigned successfully.'}, status=status.HTTP_200_OK)
+    
+    """  Updates the status of a specific report to in_progress, resolved, or rejected using a POST request """
+    
+    @action(detail=True, methods=['post'], url_path='change_status')
+    def change_status(self, request, pk=None):
+        report = self.get_object()
+        # Extracts the new status from the request body (JSON)
+        new_status_name = request.data.get('status')
+
+        if new_status_name not in ['In Progress', 'Resolved']:
+            return Response({'detail': 'Invalid status.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            # Fetches the ReportStatus object from the database where the name field matches the value of new_status_name.
+            new_status = ReportStatus.objects.get(name=new_status_name)
+        except ReportStatus.DoesNotExist:
+            return Response({'detail': 'Status does not exist in the system.'}, status=status.HTTP_404_NOT_FOUND)
+
+        report.status = new_status
+        report.save()
+        return Response({'detail': f'Status updated to {new_status_name}.'}, status=status.HTTP_200_OK)
 
 
 class ReportStatusViewSet(viewsets.ModelViewSet):
