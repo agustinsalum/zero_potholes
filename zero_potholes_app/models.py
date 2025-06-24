a9b79803f4dbcce7fdf0fe085bc867bc3c930952
Agustin Salum <agustinsalum92@hotmail.com>
2025-06-24 17:52:21 -0300

    Update DB schema, model fields and documentation

--- zero_potholes_app/models.py
+++ zero_potholes_app/models.py
@@ -16,7 +16,15 @@ class City(models.Model):
     def __str__(self):
         return f"{self.name}, {self.province.name}"
 
-class ReportStatus(models.Model): # In Progress, Rejected or Resolved
+class ReportStatus(models.Model):
+    # Received, In Progress, Resolved or Rejected
+    name = models.CharField(max_length=20, primary_key=True)
+
+    def __str__(self):
+        return self.name
+    
+class ReportSeverity(models.Model):
+    # Very low, Low, Moderate, High or Critical
     name = models.CharField(max_length=20, primary_key=True)
 
     def __str__(self):
@@ -27,6 +35,9 @@ class Report(models.Model):
     image = models.ImageField(upload_to='reports/')
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    citizen_first_name = models.CharField(max_length=50)
+    citizen_last_name = models.CharField(max_length=50)
+    citizen_email = models.EmailField()    
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     street_name = models.CharField(max_length=100, null=True, blank=True)
